f148780ae38ee4c0998578a3841295f683b5fac9
moskalyk <morgan.moskalyk@protonmail.ch>
2023-06-14 13:24:31 -0400

    updated example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,28 +25,46 @@ function App() {
   }
 
   const approve = async () => {
-    const batchApproveContractAddress = '0x2CF37Cbd0D660b4D403834EEe1adCaDDAF09eC22'
 
-    const batchApproveContractAddressInterface = new ethers.utils.Interface([
-      'function approveTokens(uint amountErc20) external'
+    const relayerAddress = '0x30131665Da3D3fB14C0cCeC8Db1eBDD863f0F0af'
+    const erc20Address = '0xCF12932b8a7450664e5C5499A2819804C3c08FD4'
+
+    const erc20Interface = new ethers.utils.Interface([
+      'function approve(address spender, uint256 amount) external returns (bool)'
     ])
 
     const amount = ethers.BigNumber.from("100000000000000000");
 
-    // Encode an ERC-20 token transfer to recipient of the specified amount
-    const data = batchApproveContractAddressInterface.encodeFunctionData(
-      'approveTokens', [amount]
+    // Encode an ERC-20 approval
+    const data = erc20Interface.encodeFunctionData(
+      'approve', [relayerAddress, amount]
     )
 
-    const transaction = {
-      to: batchApproveContractAddress,
+    const tx1 = {
+      to: erc20Address,
       data
+    }
+
+    const erc721Address = '0xc42ae8452f5057212a7c313589df6c9b83660aa3'
+
+    const erc721Interface = new ethers.utils.Interface([
+      'function setApprovalForAll(address operator, bool approved) external'
+    ])
+
+    // Encode an ERC-721 approval
+    const data1 = erc721Interface.encodeFunctionData(
+      'setApprovalForAll', [relayerAddress, true]
+    )
+
+    const tx2 = {
+      to: erc721Address,
+      data: data1
     }
 
     try {
     const wallet = await sequence.getWallet();
     const signer = await wallet.getSigner()
-    const txnResponse = await signer.sendTransaction(transaction)
+    const txnResponse = await signer.sendTransactionBatch([tx1, tx2])
     console.log(txnResponse)
     }catch(err){
       console.log('oops')
